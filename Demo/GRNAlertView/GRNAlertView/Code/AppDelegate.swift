2e5d0f79c9e3ebe79f4d77226328d28700eb14dd
Wojciech Frątczak <fratczak.wojciech@gmail.com>
2017-06-05 16:08:40 -0400

    Swift relase template

---
+++ Demo/GRNAlertView/GRNAlertView/Code/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GRNAlertView
+//
+//  Created by Wojciech on 2017-06-05.
+//  Copyright © 2017 Guarana. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
