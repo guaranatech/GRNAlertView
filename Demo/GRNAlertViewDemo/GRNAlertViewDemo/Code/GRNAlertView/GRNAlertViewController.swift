05c2bfc621d1042dc8f4c3f95554bbc5feb079d7
Wojciech Frątczak <fratczak.wojciech@gmail.com>
2017-06-07 11:33:42 -0400

    Configurators for initializing alert

--- Demo/GRNAlertViewDemo/GRNAlertViewDemo/Code/GRNAlertView/GRNAlertViewController.swift
+++ Demo/GRNAlertViewDemo/GRNAlertViewDemo/Code/GRNAlertView/GRNAlertViewController.swift
@@ -8,16 +8,47 @@
 
 import UIKit
 
+struct GRNAlertLayout {
+    var titleFont: UIFont? = nil
+    var subtitleFont: UIFont? = nil
+    var messageFont: UIFont? = nil
+    var buttonsFont: UIFont? = nil
+    
+    var tintColor: UIColor? = nil
+    var cornerRadius: UIColor? = nil
+}
+
+struct GRNAlertContent {
+    var title: String? = nil
+    var subtitle: String? = nil
+    var message: String? = nil
+    var image: UIImage? = nil
+}
+
 class GRNAlertViewController: UIViewController {
 
     //MARK: Private properties
-    @IBOutlet weak var alertView: UIView!
-    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet private weak var alertView: UIView!
+    @IBOutlet private weak var backgroundView: UIView!
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subtitleLabel: UILabel!
+    @IBOutlet private weak var messageLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    @IBOutlet private weak var leftButton: GRNMaterialButton!
+    @IBOutlet private weak var rightButton: GRNMaterialButton!
+    
+    // implement shadow
     
     //MARK: Public properties
-    typealias GRNAlertViewHandler = (alertController: GRNAlertViewController) -> Void
-    init(title: String, subtitle: String, content: String, leftButtonTitle: String? = nil, rightButtonTitle: String? = nil, leftButtonHandler: ) {
-        <#statements#>
+    typealias GRNAlertHandler = (_ alertController: GRNAlertViewController) -> Void
+//    init(content: GRNAlertContent, layout: GRNAlertLayout, leftButtonHandler: GRNAlertHandler, rightButtonHandler: GRNAlertHandler) {
+//        
+//    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: Lifecycle
@@ -27,15 +58,22 @@ class GRNAlertViewController: UIViewController {
         configureView()
     }
 
-    // MARK: Config
-    
-    private func configureView() {
-        
-    }
-
 }
 
-extension AlertViewController {
+private extension GRNAlertViewController {
+    
+    // MARK: Config
+    func configureView() {
+        
+    }
+    
+    @IBAction private func backgroundButtonAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension GRNAlertViewController {
     enum BackgroundType {
         case `default`, transparentLight, transparentDark, blurred
     }
